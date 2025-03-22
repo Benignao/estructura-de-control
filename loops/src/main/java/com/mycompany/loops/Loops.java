5850800d08626148d28fadcd5e4ac4de79e3af9a
Benigno Coj <bcojs@miumg.edu.gt>
2025-03-22 00:10:14 -0600

    agregando loops

---
+++ loops/src/main/java/com/mycompany/loops/Loops.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loops;
+
+/**
+ *
+ * @author brsan
+ */
+public class Loops {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
